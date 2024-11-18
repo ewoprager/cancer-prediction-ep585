85b9b48ffa5bd1a94f4612e233137e3cc3eeff63
ewoprager <48879954+ewoprager@users.noreply.github.com>
2024-11-18 15:17:43 +0000

    added pre-commits

--- tests/test_CancerModel.py
+++ tests/test_CancerModel.py
@@ -1,20 +1,23 @@
 import unittest
+
 from cancer_prediction.cancer_model import CancerModel
+
 
 class TestCancerModel(unittest.TestCase):
     def test_target_to_diagnosis(self):
         model = CancerModel()
         diagnosis: str = model.target_to_diagnosis(0)
-        assert(diagnosis == "Malignant")
+        assert diagnosis == "Malignant"
         diagnosis = model.target_to_diagnosis(1)
-        assert(diagnosis == "Benign")
-        
+        assert diagnosis == "Benign"
+
     def test_diagnosis_to_target(self):
         model = CancerModel()
         target: int = model.diagnosis_to_target("Benign")
-        assert(target == 1)
+        assert target == 1
         target = model.diagnosis_to_target("Malignant")
-        assert(target == 0)
+        assert target == 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
